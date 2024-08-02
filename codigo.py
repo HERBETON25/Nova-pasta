54bc59e10843e82512d90fa701079d1bc780dbaa
HERBETON25 <herbeton25@gmail.com>
2024-08-02 20:02:38 -0300

    ExercicioTurma717

---
+++ codigo.py
@@ -0,0 +1,9 @@
+
+numero = int(input('Digite um número: '))
+if numero > 0:
+    print(f'Você digitou {numero} este Número é Positivo!')
+elif numero == 0:
+    print(f'Você digitou {numero} este Número é Zero!')
+else:
+    print(f'Você digitou {numero} este Número é Negativo!')
+    
